f96716a6f2e05d8f005253c06037105af8a9a4ec
mostafa <mustafa.bahr12323@gmail.com>
2023-10-11 15:55:19 +0300

    SliverFillRemaining

--- lib/features/home/presentation/views/widgets/book_details_view_body.dart
+++ lib/features/home/presentation/views/widgets/book_details_view_body.dart
@@ -13,45 +13,52 @@ class BookViewDetailsBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return   Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 20),
-      child: SingleChildScrollView(
-        child: Column(
-          children: [
-            const CustomBookDetailsAppBar(),
-              Padding(
-                padding: EdgeInsets.symmetric(horizontal:MediaQuery.of(context).size.width*0.2,
-                vertical: MediaQuery.of(context).size.height*0.04
-              ),
-                child: const CustomBookImage(),
-              ),
-            const Text("Book 1",
-            style: AppStyles.textStyle30,),
-            const SizedBox(height: 10,),
-              Text("Book 1 description",
-              style: AppStyles.textStyle18.copyWith(
-                fontWeight: FontWeight.normal,
-                fontStyle: FontStyle.italic,
-                color: Colors.grey,
-              ),),
-            const SizedBox(height: 18,),
-            const BookingRating(),
-            const SizedBox(height: 18,),
-            const ButtonAction(),
-            const SizedBox(height: 18,),
-            Align(
-              alignment: Alignment.topLeft,
-              child: Text("You can also like ",
-                style: AppStyles.textStyle14.copyWith(
-                  fontWeight: FontWeight.normal,
-                ),),
+    return   CustomScrollView(
+      slivers: [
+        SliverFillRemaining(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20),
+            child: Column(
+              children: [
+                const CustomBookDetailsAppBar(),
+                Padding(
+                  padding: EdgeInsets.symmetric(horizontal:MediaQuery.of(context).size.width*0.2,
+                      vertical: MediaQuery.of(context).size.height*0.04
+                  ),
+                  child: const CustomBookImage(),
+                ),
+                const Text("Book 1",
+                  style: AppStyles.textStyle30,),
+                const SizedBox(height: 10,),
+                Text("Book 1 description",
+                  style: AppStyles.textStyle18.copyWith(
+                    fontWeight: FontWeight.normal,
+                    fontStyle: FontStyle.italic,
+                    color: Colors.grey,
+                  ),),
+                const SizedBox(height: 18,),
+                const BookingRating(),
+                const SizedBox(height: 18,),
+                const ButtonAction(),
+                const Expanded(
+                  child:  SizedBox(height: 18,),
+                ),
+                Align(
+                  alignment: Alignment.topLeft,
+                  child: Text("You can also like ",
+                    style: AppStyles.textStyle14.copyWith(
+                      fontWeight: FontWeight.normal,
+                    ),),
+                ),
+                const SizedBox(height: 18,),
+                const SimilarBooksListView(),
+                const SizedBox(height: 18,),
+              ],
             ),
-            const SizedBox(height: 18,),
-            const SimilarBooksListView(),
-            
-          ],
+          ),
         ),
-      ),
+
+      ],
     );
   }
 }
