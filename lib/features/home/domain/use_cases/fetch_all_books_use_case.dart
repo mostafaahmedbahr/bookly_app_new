cac96efbd0e6d76f4a5c9ef0f7a5d538e43bced2
mostafa <mustafa.bahr12323@gmail.com>
2023-10-12 10:34:29 +0300

    use cases 2

--- lib/features/home/domain/use_cases/fetch_all_books_use_case.dart
+++ lib/features/home/domain/use_cases/fetch_all_books_use_case.dart
@@ -1,17 +1,18 @@
+import 'package:bookly_app/core/use_cases/use_case_with_no_param.dart';
 import 'package:bookly_app/features/home/domain/repos/home_repo.dart';
 import 'package:dartz/dartz.dart';
 
 import '../../../../core/errors/error_class.dart';
 import '../entites/book_entity.dart';
 
-class FetchAllBooksUseCase
+class FetchAllBooksUseCase extends UseCaseWithNoParam<List<BookEntity>>
 {
   final HomeRepo homeRepo;
   FetchAllBooksUseCase(this.homeRepo);
 
+  @override
+  Future<Either<Errors, List<BookEntity>>> call() async {
+    return await homeRepo.fetchAllBooks();
 
-  Future<Either< Errors, List<BookEntity>>> fetchAllBooks(){
-
-    return homeRepo.fetchAllBooks();
   }
 }
--- lib/features/home/domain/use_cases/fetch_newest_books_use_case.dart
+++ lib/features/home/domain/use_cases/fetch_newest_books_use_case.dart
@@ -1,17 +1,34 @@
+import 'package:bookly_app/core/use_cases/use_case_with_no_param.dart';
 import 'package:bookly_app/features/home/domain/repos/home_repo.dart';
 import 'package:dartz/dartz.dart';
 
 import '../../../../core/errors/error_class.dart';
 import '../entites/book_entity.dart';
 
-class FetchNewestUseCase
-{
+// class FetchNewestUseCase
+// {
+//   final HomeRepo homeRepo;
+//   FetchNewestUseCase(this.homeRepo);
+//
+//
+//   // ممكن نسميها اى اسم معبر عشان اللغبطة
+//   // call
+//   //fetchNewestBooks
+//   Future<Either< Errors, List<BookEntity>>> call() async{
+//
+//     return await homeRepo.fetchNewestBooks();
+//   }
+// }
+
+class FetchNewestUseCase extends UseCaseWithNoParam<List<BookEntity>> {
   final HomeRepo homeRepo;
+
   FetchNewestUseCase(this.homeRepo);
 
+  @override
+  Future<Either<Errors, List<BookEntity>>> call()async {
 
-  Future<Either< Errors, List<BookEntity>>> fetchNewestBooks(){
+    return await homeRepo.fetchNewestBooks();
 
-    return homeRepo.fetchNewestBooks();
   }
 }
