50bf02d04adbc1bba2f3a2c607bcc0d3a34e8a1d
mostafa <mustafa.bahr12323@gmail.com>
2023-10-26 10:37:07 +0300

    errors

--- lib/core/errors/error_class.dart
+++ lib/core/errors/error_class.dart
@@ -1,6 +1,49 @@
-abstract class Errors{}
+import 'package:dio/dio.dart';
 
-class ServerError extends Errors{}
+abstract class Errors {
+  final String error;
 
-class NetworkError extends Errors{}
-class CachedError extends Errors{}
+  Errors({required this.error});
+}
+
+class ServerError extends Errors {
+  ServerError({required super.error});
+
+  factory ServerError.fromDioError(DioException dioException) {
+    switch (dioException.type) {
+      case DioExceptionType.connectionTimeout:
+        return ServerError(error: "connectionTimeout");
+
+      case DioExceptionType.sendTimeout:
+        return ServerError(error: "sendTimeout");
+
+      case DioExceptionType.receiveTimeout:
+        return ServerError(error: "receiveTimeout");
+
+      case DioExceptionType.badCertificate:
+        return ServerError(error: "badCertificate");
+
+        // فيها شغل
+      case DioExceptionType.badResponse:
+        if(dioException.response!.statusCode==404){}
+        return ServerError(error: "badResponse");
+
+      case DioExceptionType.cancel:
+        return ServerError(error: "cancel");
+
+      case DioExceptionType.connectionError:
+        return ServerError(error: "connectionError");
+
+      case DioExceptionType.unknown:
+        return ServerError(error: "unknown");
+    }
+  }
+}
+
+class NetworkError extends Errors {
+  NetworkError({required super.error});
+}
+
+class CachedError extends Errors {
+  CachedError({required super.error});
+}
--- lib/features/home/data/repos/home_repo_implementation.dart
+++ lib/features/home/data/repos/home_repo_implementation.dart
@@ -4,43 +4,50 @@ import 'package:bookly_app/features/home/data/data_source/home_remote_data_sourc
 import 'package:bookly_app/features/home/domain/entites/book_entity.dart';
 import 'package:bookly_app/features/home/domain/repos/home_repo.dart';
 import 'package:dartz/dartz.dart';
+import 'package:dio/dio.dart';
 
-class HomeRepoImplementation extends HomeRepo
-{
+class HomeRepoImplementation extends HomeRepo {
   final HomeRemoteDataSource homeRemoteDataSource;
   final HomeLocalDataSource homeLocalDataSource;
 
-  HomeRepoImplementation({required this.homeRemoteDataSource,required this.homeLocalDataSource});
+  HomeRepoImplementation(
+      {required this.homeRemoteDataSource, required this.homeLocalDataSource});
+
   @override
-  Future<Either<Errors, List<BookEntity>>> fetchAllBooks() async{
-    try{
-      dynamic books ;
+  Future<Either<Errors, List<BookEntity>>> fetchAllBooks() async {
+    try {
+      dynamic books;
       books = homeLocalDataSource.fetchAllBooks();
-      if(books.isNotEmpty){
+      if (books.isNotEmpty) {
         return right(books);
       }
-        books = homeRemoteDataSource.fetchAllBooks();
+      books = homeRemoteDataSource.fetchAllBooks();
       return right(books);
-    }catch(error)
-    {
-      return left(ServerError());
+    } catch (error) {
+      if (error is DioException) {
+        return left(ServerError.fromDioError(error));
+      } else {
+        return left(ServerError(error: error.toString()));
+      }
     }
   }
 
   @override
-  Future<Either<Errors, List<BookEntity>>> fetchNewestBooks() async{
-    try{
-      dynamic books ;
-         books = homeLocalDataSource.fetchNewestBooks();
-      if( books.isNotEmpty){
-        return right( books);
+  Future<Either<Errors, List<BookEntity>>> fetchNewestBooks() async {
+    try {
+      dynamic books;
+      books = homeLocalDataSource.fetchNewestBooks();
+      if (books.isNotEmpty) {
+        return right(books);
       }
-        books = homeRemoteDataSource.fetchNewestBooks();
+      books = homeRemoteDataSource.fetchNewestBooks();
       return right(books);
-    }catch(error)
-    {
-      return left(ServerError());
+    } catch (error) {
+      if (error is DioException) {
+        return left(ServerError.fromDioError(error));
+      } else {
+        return left(ServerError(error: error.toString()));
+      }
     }
   }
-
 }
